046dfa3129be4002b8af7938790e6546758c03a7
Lukari <lukari87@gmail.com>
2024-01-23 09:17:55 +0700

    Update Bai2.java

--- Lab3/src/Bai2.java
+++ Lab3/src/Bai2.java
@@ -1,4 +1,4 @@
-public class Bai2BangCuuChuong {
+public class Bai2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 10; j++) {
